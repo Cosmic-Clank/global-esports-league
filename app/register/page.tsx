70fee8f2d8fe0d53f9ef19bf67b23b56b8892e0e
Tayyab Sajid <tayyabsajidq41321@gmail.com>
2025-11-14 13:15:38 +0400

    socials and registration page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,32 @@
+import CustomButton from "@/components/CustomButton";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
 
 export const metadata = {
 	title: "Register - Gulf Esports League",
 };
+
+const tourneys = [
+	{
+		name: "Valorant 2v2",
+		description: "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet.",
+		image: "/images/valorant-cover.jpg",
+		href: "/home",
+	},
+	{
+		name: "Valorant 5v5",
+		description: "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet.",
+		image: "/images/standard-league.png",
+		href: "/home",
+	},
+	{
+		name: "League Of Legends",
+		description: "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet.",
+		image: "/images/league-of-legends-cover.jpg",
+		href: "/home",
+	},
+];
 
 export default function RegisterPage() {
 	return (
@@ -19,13 +42,19 @@ export default function RegisterPage() {
 				<h1 className='text-3xl text-center md:text-4xl font-bold'>Register for the tournament</h1>
 				<p className='text-center text-lg mb-8 leading-relaxed'>Sign up now and secure your spot in our upcoming Esports events.</p>
 			</div>
-
-			<div className='relative z-10 max-w-4xl mx-auto w-full px-6 pb-16'>
-				<div className='w-full h-[70vh] md:h-[80vh] rounded overflow-hidden shadow-lg bg-(--color-secondary-bg)'>
-					<iframe src='https://docs.google.com/forms/d/e/1FAIpQLScp5BWCOWM0tfCDudUbhGZIsdZCfT7fSipAb7BhShCjYNdjUQ/viewform?embedded=true' className='w-full h-full border-none' title='GEL Registration Form'>
-						Loading…
-					</iframe>
-				</div>
+			<div className='max-w-4xl mx-auto space-y-8'>
+				{tourneys.map((tourney, index) => {
+					return (
+						<div key={index} className={`flex justify-between relative w-full bg-(--color-secondary-bg) rounded-3xl p-9 gap-8 ${index % 2 == 0 ? "flex-row" : "flex-row-reverse"}`}>
+							<div className='flex flex-col'>
+								<h3 className='text-3xl tracking-wide mb-4'>{tourney.name}</h3>
+								<p className='text-gray-400 text-lg'>{tourney.description}</p>
+								<CustomButton href={tourney.href} text='Register Now' />
+							</div>
+							<Image className='w-72 rounded-xl' src={tourney.image} width={216} height={216} alt={`image for ${tourney.name} tournament`} />
+						</div>
+					);
+				})}
 			</div>
 
 			<Footer />
--- components/Socials.tsx
+++ components/Socials.tsx
@@ -5,21 +5,31 @@ export default function Socials() {
 	return (
 		<section id='socials' className='mx-auto max-w-6xl px-6 py-12'>
 			<FadeUp stagger className='flex flex-row items-center justify-center gap-12'>
-				<a href='https://www.instagram.com/'>
-					<Instagram color='gray' />
-				</a>
-				<a href='https://www.instagram.com/'>
-					<Facebook color='gray' />
-				</a>
-				<a href='https://www.instagram.com/'>
-					<Twitter color='gray' />
-				</a>
-				<a href='https://www.instagram.com/'>
-					<Twitch color='gray' />
-				</a>
-				<a href='https://www.instagram.com/'>
-					<Youtube color='gray' />
-				</a>
+				<div className='hover:-translate-y-1.5 transition-transform duration-200'>
+					<a href='https://www.instagram.com/'>
+						<Instagram className='hover:text-pink-700 text-gray-500 transition-colors duration-200' />
+					</a>
+				</div>
+				<div className='hover:-translate-y-1.5 transition-transform duration-200'>
+					<a href='https://www.instagram.com/'>
+						<Facebook className='hover:text-blue-700 text-gray-500 transition-colors duration-200' />
+					</a>
+				</div>
+				<div className='hover:-translate-y-1.5 transition-transform duration-200'>
+					<a href='https://www.instagram.com/'>
+						<Twitter className='hover:text-blue-400 text-gray-500 transition-colors duration-200' />
+					</a>
+				</div>
+				<div className='hover:-translate-y-1.5 transition-transform duration-200'>
+					<a href='https://www.instagram.com/'>
+						<Twitch className='hover:text-purple-700 text-gray-500 transition-colors duration-200' />
+					</a>
+				</div>
+				<div className='hover:-translate-y-1.5 transition-transform duration-200'>
+					<a href='https://www.instagram.com/'>
+						<Youtube className='hover:text-red-700 text-gray-500 transition-colors duration-200' />
+					</a>
+				</div>
 			</FadeUp>
 		</section>
 	);
